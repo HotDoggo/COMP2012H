a9776eabb999cf1c9be7be3745f6cc4223153ffd
HotDoggo <pavedewa@gmail.com>
2023-11-20 22:53:56 +0800

    lab7: functoins done, memory leak found

--- Labs/lab7/order_item.cpp
+++ Labs/lab7/order_item.cpp
@@ -2,7 +2,7 @@
  * @file order_item.cpp
  * @brief Definitions of member and helper functions for OrderItem and derived classes.
  *
- * This file defines member functions of OrderItem, Food, Burger, Fries, Combo, and 
+ * This file defines member functions of OrderItem, Food, Burger, Fries, Combo, and
  * corresponding helper factory functions for ordering them.
  */
 
@@ -15,29 +15,32 @@
 /**
  * Prepare this item.
  */
-void OrderItem::prepare() {
+void OrderItem::prepare()
+{
 	std::cout << "Preparing " << get_name() << "..." << std::endl;
 }
 
 /**
  * Protected function for derived classes to modify name.
  */
-void Food::set_name(std::string name) {
+void Food::set_name(std::string name)
+{
 	this->name = name;
 }
-
 
 /**
  * Override get_name to return name field.
  */
-std::string Food::get_name() const {
+std::string Food::get_name() const
+{
 	return this->name;
 }
 
 /**
  * Override get_price to return price field.
  */
-int Food::get_price() const {
+int Food::get_price() const
+{
 	return this->price;
 }
 
@@ -47,11 +50,13 @@ int Food::get_price() const {
  *   Set name and price of the inherited Food class.
  */
 //=========== TODO ===================================================
-
-
-
-
-
+Burger::Burger(std::string name, int price, std::string modifier) : Food(name, price), modifier(modifier)
+{
+	if (!modifier.empty())
+	{
+		set_name(name + " (" + modifier + ")");
+	}
+}
 
 //--------------------------------------------------------------------
 
@@ -64,7 +69,8 @@ int Food::get_price() const {
  *   If None is selected, or selection is not in range, return nullptr.
  *   Otherwise, a second selection is prompted for additional request, which is put into modifier.
  */
-Burger* order_burger() {
+Burger *order_burger()
+{
 	std::cout << "================ Burger =================" << std::endl;
 	const std::vector<std::string> options = {
 		"Beef Burger",
@@ -72,8 +78,9 @@ Burger* order_burger() {
 		"None",
 	};
 
-	for (int i = 0; i < options.size(); i++) {
-		std::cout << std::setw(3) << i+1 << ". " << options[i] << std::endl;
+	for (int i = 0; i < options.size(); i++)
+	{
+		std::cout << std::setw(3) << i + 1 << ". " << options[i] << std::endl;
 	}
 	std::cout << "-----------------------------------------" << std::endl;
 	std::cout << "Please input the index of your selection: ";
@@ -82,22 +89,26 @@ Burger* order_burger() {
 	std::cout << std::endl;
 
 	std::string modifier = "";
-	if (index >= 1 && index < options.size()) {
+	if (index >= 1 && index < options.size())
+	{
 		std::cout << "-----------------------------------------" << std::endl;
 		std::cout << "Any special request for your burger? If not, type 'None': ";
 		std::getline(std::cin, modifier); // This is intentional due to leftover '\n'
 		std::getline(std::cin, modifier);
-		if (modifier == "None") modifier = "";
+		if (modifier == "None")
+			modifier = "";
 		std::cout << std::endl;
 	}
 
 	//=========== TODO ===================================================
+	const std::vector<int> price = {
+		21,
+		24};
 
+	if (index >= 1 && index < options.size())
+		return new Burger(options[index - 1], price[index - 1], modifier);
 
-
-
-
-	
+	return nullptr;
 	//--------------------------------------------------------------------
 }
 
@@ -109,11 +120,20 @@ Burger* order_burger() {
  * 	 If the size is Large, modify name to add " (Large)" after the name.
  */
 //=========== TODO ===================================================
-
-
-
-
-
+Fries::Fries(std::string name, int price, FriesSize size) : Food(name, price), size(size)
+{
+	switch (size)
+	{
+	case FriesSize::Small:
+		set_name(name + " (Small)");
+		break;
+	case FriesSize::Large:
+		set_name(name + " (Large)");
+		break;
+	default:
+		break;
+	}
+}
 //--------------------------------------------------------------------
 
 /**
@@ -123,13 +143,18 @@ Burger* order_burger() {
  * 	 If the size is Large, the price should be increased by 5.
  */
 //=========== TODO ===================================================
-
-
-
-
-
-
-
+int Fries::get_price() const
+{
+	switch (size)
+	{
+	case FriesSize::Small:
+		return Food::get_price() - 3;
+	case FriesSize::Large:
+		return Food::get_price() + 5;
+	default:
+		return Food::get_price();
+	}
+}
 //--------------------------------------------------------------------
 
 /**
@@ -141,7 +166,8 @@ Burger* order_burger() {
  *   If None is selected, or selection is not in range, return nullptr.
  *   Otherwise, a second selection is prompted to choose size. sizeIndex contains the selection.
  */
-Fries* order_fries() {
+Fries *order_fries()
+{
 	std::cout << "================= Fries =================" << std::endl;
 	const std::vector<std::string> options = {
 		"Regular Fries",
@@ -149,8 +175,9 @@ Fries* order_fries() {
 		"None",
 	};
 
-	for (int i = 0; i < options.size(); i++) {
-		std::cout << std::setw(3) << i+1 << ". " << options[i] << std::endl;
+	for (int i = 0; i < options.size(); i++)
+	{
+		std::cout << std::setw(3) << i + 1 << ". " << options[i] << std::endl;
 	}
 	std::cout << "-----------------------------------------" << std::endl;
 	std::cout << "Please input the index of your selection: ";
@@ -159,7 +186,8 @@ Fries* order_fries() {
 	std::cout << std::endl;
 
 	int sizeIndex = 2;
-	if (index >= 1 && index < options.size()) {
+	if (index >= 1 && index < options.size())
+	{
 		std::cout << "-----------------------------------------" << std::endl;
 		std::cout << "Select the size of your fries: " << std::endl;
 
@@ -169,22 +197,28 @@ Fries* order_fries() {
 			"Large",
 		};
 
-		for (int i = 0; i < options.size(); i++) {
-			std::cout << std::setw(3) << i+1 << ". " << options[i] << std::endl;
+		for (int i = 0; i < options.size(); i++)
+		{
+			std::cout << std::setw(3) << i + 1 << ". " << options[i] << std::endl;
 		}
 
 		std::cout << "-----------------------------------------" << std::endl;
 		std::cout << "Please input the index of your selection: ";
 		std::cin >> sizeIndex;
-		if (sizeIndex < 1 || sizeIndex > 3) sizeIndex = 2;
+		if (sizeIndex < 1 || sizeIndex > 3)
+			sizeIndex = 2;
 		std::cout << std::endl;
 	}
 
 	//=========== TODO ===================================================
+	const std::vector<int> price = {
+		10,
+		15};
 
+	if (index >= 1 && index < options.size())
+		return new Fries(options[index - 1], price[index - 1], static_cast<FriesSize>(sizeIndex));
 
-
-	
+	return nullptr;
 	//--------------------------------------------------------------------
 }
 
@@ -195,9 +229,11 @@ Fries* order_fries() {
  *   If the drink is iced, add "Iced " to the beginning of name.
  */
 //=========== TODO ===================================================
-
-
-
+Drink::Drink(std::string name, int price, bool iced) : Food(name, price), iced(iced)
+{
+	if (iced)
+		set_name("Iced " + name);
+}
 //--------------------------------------------------------------------
 
 /**
@@ -206,10 +242,12 @@ Fries* order_fries() {
  *   If the drink is iced, the price should be increased by 4.
  */
 //=========== TODO ===================================================
-
-
-
-
+int Drink::get_price() const
+{
+	if (iced)
+		return Food::get_price() + 4;
+	return Food::get_price();
+}
 //--------------------------------------------------------------------
 
 /**
@@ -219,10 +257,11 @@ Fries* order_fries() {
  *   Coca-Cola has name "Coca-Cola" and price is 5.
  *   Cappuccino has name "Cappuccino" and price is 13.
  *   If None is selected, or selection is not in range, return nullptr.
- *   Otherwise, a second selection is prompted to choose whether the drink is iced. 
+ *   Otherwise, a second selection is prompted to choose whether the drink is iced.
  *   It is put into the boolean iced.
  */
-Drink* order_drink() {
+Drink *order_drink()
+{
 	std::cout << "================= Drink =================" << std::endl;
 	const std::vector<std::string> options = {
 		"Coca-Cola",
@@ -230,8 +269,9 @@ Drink* order_drink() {
 		"None",
 	};
 
-	for (int i = 0; i < options.size(); i++) {
-		std::cout << std::setw(3) << i+1 << ". " << options[i] << std::endl;
+	for (int i = 0; i < options.size(); i++)
+	{
+		std::cout << std::setw(3) << i + 1 << ". " << options[i] << std::endl;
 	}
 	std::cout << "-----------------------------------------" << std::endl;
 	std::cout << "Please input the index of your selection: ";
@@ -240,7 +280,8 @@ Drink* order_drink() {
 	std::cout << std::endl;
 
 	bool iced = false;
-	if (index >= 1 && index < options.size()) {
+	if (index >= 1 && index < options.size())
+	{
 		std::cout << "-----------------------------------------" << std::endl;
 		std::cout << "Would you like your drink iced? " << std::endl;
 
@@ -249,8 +290,9 @@ Drink* order_drink() {
 			"No",
 		};
 
-		for (int i = 0; i < options.size(); i++) {
-			std::cout << std::setw(3) << i+1 << ". " << options[i] << std::endl;
+		for (int i = 0; i < options.size(); i++)
+		{
+			std::cout << std::setw(3) << i + 1 << ". " << options[i] << std::endl;
 		}
 
 		std::cout << "-----------------------------------------" << std::endl;
@@ -262,38 +304,51 @@ Drink* order_drink() {
 	}
 
 	//=========== TODO ===================================================
+	const std::vector<int> price = {
+		5,
+		13};
 
+	if (index >= 1 && index < options.size())
+		return new Drink(options[index - 1], price[index - 1], iced);
 
-
-
-
-	
+	return nullptr;
 	//--------------------------------------------------------------------
 }
-
 
 /**
  * Member functions for the Combo class.
  */
 //=========== TODO ===================================================
+Combo::Combo(Burger *burger, Fries *fries, Drink *drink)
+{
+	std::vector<std::string> name_list;
+	int price = 0;
+	if (burger)
+	{
+		name_list.push_back(burger->get_name());
+		price += burger->get_price();
+	}
+	if (fries)
+	{
+		name_list.push_back(fries->get_name());
+		price += fries->get_price();
+	}
+	if (drink)
+	{
+		name_list.push_back(drink->get_name());
+		price += drink->get_price();
+	}
 
+	std::string name = "Combo ";
+	for (int i = 0; i < name_list.size() - 1; i++)
+	{
+		name += name_list[i] + " & ";
+	}
+	name += name_list.back();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	this->name = name;
+	this->price = price - (price / 10);
+}
 //--------------------------------------------------------------------
 
 /**
@@ -302,27 +357,35 @@ Drink* order_drink() {
  *   If at least than 2 items are selected, return a new Combo instance with the selected items.
  *   (otherwise, someone could order a single item and get a discount!)
  */
-Combo* order_combo() {
+Combo *order_combo()
+{
 	std::cout << "Ordering a combo..." << std::endl;
 	std::cout << "Select a burger" << std::endl;
-	Burger* burger = order_burger();
+	Burger *burger = order_burger();
 
 	std::cout << "Select fries" << std::endl;
-	Fries* fries = order_fries();
+	Fries *fries = order_fries();
 
 	std::cout << "Select a drink" << std::endl;
-	Drink* drink = order_drink();
+	Drink *drink = order_drink();
 
 	//=========== TODO ===================================================
+	int num_items = 0;
+	if (burger)
+		num_items++;
+	if (fries)
+		num_items++;
+	if (drink)
+		num_items++;
 
+	if (num_items < 2)
+	{
+		std::cout << "Insufficient number of items. Order cancelled." << std::endl;
+		return nullptr;
+	}
 
-
-
-	
+	return new Combo(burger, fries, drink);
 	//--------------------------------------------------------------------
-
-	std::cout << "Insufficient number of items. Order cancelled." << std::endl;
-	return nullptr;
 }
 
 /**
@@ -333,7 +396,8 @@ Combo* order_combo() {
  *   selection part.
  *   If None is selected, or selection is not in range, return nullptr.
  */
-OrderItem* order_item() {
+OrderItem *order_item()
+{
 	std::cout << "================= Menu ==================" << std::endl;
 	const std::vector<std::string> options = {
 		"Combo",
@@ -343,8 +407,9 @@ OrderItem* order_item() {
 		"None",
 	};
 
-	for (int i = 0; i < options.size(); i++) {
-		std::cout << std::setw(3) << i+1 << ". " << options[i] << std::endl;
+	for (int i = 0; i < options.size(); i++)
+	{
+		std::cout << std::setw(3) << i + 1 << ". " << options[i] << std::endl;
 	}
 	std::cout << "-----------------------------------------" << std::endl;
 	std::cout << "Please input the index of your selection: ";
@@ -352,24 +417,26 @@ OrderItem* order_item() {
 	std::cin >> index;
 	std::cout << std::endl;
 
-	OrderItem* item;
-	switch (index) {
-		case 1:
-			item = order_combo();
-			break;
-		case 2:
-			item = order_burger();
-			break;
-		case 3:
-			item = order_fries();
-			break;
-		case 4:
-			item = order_drink();
-			break;
-		default:
-			return nullptr;
-    }
-	if (item == nullptr) {
+	OrderItem *item;
+	switch (index)
+	{
+	case 1:
+		item = order_combo();
+		break;
+	case 2:
+		item = order_burger();
+		break;
+	case 3:
+		item = order_fries();
+		break;
+	case 4:
+		item = order_drink();
+		break;
+	default:
+		return nullptr;
+	}
+	if (item == nullptr)
+	{
 		return order_item();
 	}
 	return item;
--- Labs/lab7/order_item.h
+++ Labs/lab7/order_item.h
@@ -23,29 +23,29 @@
  * The Abstract class OrderItem for general order items including a single food or a combo set.
  * It supports getting name and price, and preparing.
  */
-class OrderItem {
-	public:
-		//The default virtual destructor.
-		virtual ~OrderItem() = default;
+class OrderItem
+{
+public:
+	// The default virtual destructor.
+	virtual ~OrderItem() = default;
 
-		//Return the name for this item.
-		//To be overridden.
-		virtual std::string get_name() const = 0;
+	// Return the name for this item.
+	// To be overridden.
+	virtual std::string get_name() const = 0;
 
-		//Return the price for this item.
-		//To be overridden.
-		virtual int get_price() const = 0;
+	// Return the price for this item.
+	// To be overridden.
+	virtual int get_price() const = 0;
 
-		//Prepare this item.
-		void prepare();
+	// Prepare this item.
+	void prepare();
 };
 
 /**
  * A factory function for making a concrete order item.
  * Return nullptr if cancelled.
  */
-OrderItem* order_item();
-
+OrderItem *order_item();
 
 //======================================//
 //				   Food	    			//
@@ -56,27 +56,27 @@ OrderItem* order_item();
  * It inherits the OrderItem class.
  * It keeps the name and price directly as they are known on creation.
  */
-class Food: public OrderItem {
-	//CANNOT be accessed by derived classes.
-	private:
-		std::string name;
-		int price = 0;
+class Food : public OrderItem
+{
+	// CANNOT be accessed by derived classes.
+private:
+	std::string name;
+	int price = 0;
 
-	//CAN be accessed by derived classes.
-	protected:
-		void set_name(std::string name);
+	// CAN be accessed by derived classes.
+protected:
+	void set_name(std::string name);
 
-	public:
-		//Constructor
-		Food(std::string name, int price): name(name), price(price) {}
+public:
+	// Constructor
+	Food(std::string name, int price) : name(name), price(price) {}
 
-		//Override get_name to return name field.
-		virtual std::string get_name() const override;
+	// Override get_name to return name field.
+	virtual std::string get_name() const override;
 
-		//Override get_price to return price field.
-		virtual int get_price() const override;
+	// Override get_price to return price field.
+	virtual int get_price() const override;
 };
-
 
 //======================================//
 //				  Burger				//
@@ -86,31 +86,32 @@ class Food: public OrderItem {
  * The Burger class for burger orders.
  * It inherits the Food class.
  */
-class Burger: public Food {
-	private:
-		std::string modifier;
+class Burger : public Food
+{
+private:
+	std::string modifier;
 
-	public:
-		//Constructor, create a burger order.
-		Burger(std::string name, int price, std::string modifier = "");
+public:
+	// Constructor, create a burger order.
+	Burger(std::string name, int price, std::string modifier = "");
 };
 
 /**
  * A factory function for making an order of burger.
  * Return nullptr if cancelled.
  */
-Burger* order_burger();
-
+Burger *order_burger();
 
 //======================================//
 //				   Fries				//
 //======================================//
 
-//Supported fries size.
-enum class FriesSize {
-	Small = 1, //Small size.
-	Regular, //Regular size.
-	Large, //Enlarged size.
+// Supported fries size.
+enum class FriesSize
+{
+	Small = 1, // Small size.
+	Regular,   // Regular size.
+	Large,	   // Enlarged size.
 };
 
 /**
@@ -118,25 +119,25 @@ enum class FriesSize {
  * It inherits the Food class.
  * It keeps the size for this order of fries.
  */
-class Fries: public Food {
-	private:
-		//size
-		FriesSize size;
+class Fries : public Food
+{
+private:
+	// size
+	FriesSize size;
 
-	public:
-		//Constructor, create a fries order with size.
-		Fries(std::string name, int price, FriesSize size = FriesSize::Regular);
+public:
+	// Constructor, create a fries order with size.
+	Fries(std::string name, int price, FriesSize size = FriesSize::Regular);
 
-		//Override get_price for custom drink price.
-		virtual int get_price() const override final;
+	// Override get_price for custom drink price.
+	virtual int get_price() const override final;
 };
 
 /**
  * A factory function for making an order of fries.
  * Return nullptr if cancelled.
  */
-Fries* order_fries();
-
+Fries *order_fries();
 
 //======================================//
 //				   Drink				//
@@ -148,25 +149,25 @@ Fries* order_fries();
  * It keeps the type for this order of drink.
  * The drink can either be hot or iced.
  */
-class Drink: public Food {
-	private:
-		//iced
-		bool iced;
+class Drink : public Food
+{
+private:
+	// iced
+	bool iced;
 
-	public:
-		//Constructor, create a hot or iced drink order with type.
-		Drink(std::string name, int price, bool iced = false);
+public:
+	// Constructor, create a hot or iced drink order with type.
+	Drink(std::string name, int price, bool iced = false);
 
-		//Override get_price for custom drink price.
-		virtual int get_price() const override final;
+	// Override get_price for custom drink price.
+	virtual int get_price() const override final;
 };
 
 /**
  * A factory function for making an order of drink.
  * Return nullptr if cancelled.
  */
-Drink* order_drink();
-
+Drink *order_drink();
 
 //======================================//
 //				   Combo				//
@@ -182,27 +183,30 @@ Drink* order_drink();
  *   Requirements:
  *     There must be constructor of type Combo(Burger* burger, Fries* fries, Drink* drink).
  *
- *     The name for a combo of burger a, fries b and drink c is 
+ *     The name for a combo of burger a, fries b and drink c is
  *     "Combo " followed by names of a, b and c separated by " & ".
  *     For example, the name of the combo of the burger named "Beef Burger" and the fries named "Twisted Fries"
  *     is "Combo Beef Burger & Twisted Fries".
  *
- *     The price for a combo of burger a, fries b and drink c is the total price of a, b and c with 10% discount (the discount is floored). 
+ *     The price for a combo of burger a, fries b and drink c is the total price of a, b and c with 10% discount (the discount is floored).
  *     For example, the price of the combo of a 30 HKD burger and a 18 HKD fries
  *     is 30 + 18 - floor(0.1*(30 + 18)) = 44HKD.
  */
 //=========== TODO ===================================================
+class Combo : public OrderItem
+{
+private:
+	std::string name;
+	int price;
 
+public:
+	Combo(Burger *burger, Fries *fries, Drink *drink);
 
+	virtual std::string get_name() const override final { return this->name; };
 
-
-
-
-
-
-
-
+	virtual int get_price() const override final { return this->price; };
+};
 //--------------------------------------------------------------------
 
-Combo* order_combo();
+Combo *order_combo();
 #endif //__ORDER_ITEM_H__
