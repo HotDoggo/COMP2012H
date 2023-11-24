11e69f5a0275136e48374d8488c5f3910e8c2492
HotDoggo <pavedewa@gmail.com>
2023-11-24 17:47:32 +0800

    PA3: minor fix

--- PA/pa3/main.cpp
+++ PA/pa3/main.cpp
@@ -6,34 +6,41 @@
 #include "utils.h"
 using namespace std;
 
-template<typename T>
-void read_eval_print_loop() {
+template <typename T>
+void read_eval_print_loop()
+{
 	string input;
-	Expr<T>* expr_ptr;
-	while(true) {
-		//prompt
+	Expr<T> *expr_ptr;
+	while (true)
+	{
+		// prompt
 		cout << ">>> ";
 		getline(cin, input);
 
-		//check if exit
+		// check if exit
 		input = trim(input);
-		if (input == "exit") {
+		if (input == "exit")
+		{
 			break;
 		}
 
 		expr_ptr = parse<T>(input);
-		if (expr_ptr != nullptr) {
+		if (expr_ptr != nullptr)
+		{
 			cout << "Your input is parsed as:" << endl;
 			cout << "    " << expr_ptr->as_string() << endl;
 			cout << "It is evaluated as: " << to_str(expr_ptr->eval()) << "." << endl;
 			delete expr_ptr;
-		} else {
+		}
+		else
+		{
 			cout << "I don't understand your input." << endl;
 		}
 	}
 }
 
-void usage_info() {
+void usage_info()
+{
 	cout << "This is a basic calculator written in cpp" << endl;
 	cout << "# Launch with PA3.exe [MODE]. MODE can be int, float or complex.    #" << endl;
 	cout << "# The default mode is float. Type expression to calculate.          #" << endl;
@@ -41,22 +48,31 @@ void usage_info() {
 	cout << endl;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	usage_info();
 	string mode = "float";
-	if (argc == 2) {
+	if (argc == 2)
+	{
 		mode = argv[1];
 	}
-	if (mode == "float") {
+	if (mode == "float")
+	{
 		cout << "It is running in the float mode." << endl;
 		read_eval_print_loop<float>();
-	} else if (mode == "int") {
+	}
+	else if (mode == "int")
+	{
 		cout << "It is running in the int mode." << endl;
 		read_eval_print_loop<int>();
-	} else if (mode == "complex") {
+	}
+	else if (mode == "complex")
+	{
 		cout << "It is running in the complex mode." << endl;
 		read_eval_print_loop<Complex<double>>();
-	} else {
+	}
+	else
+	{
 		cout << "Invalid mode!" << endl;
 	}
 }
